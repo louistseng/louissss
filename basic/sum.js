57f84de13bb018f938b044cc80da4c6fa3a25385
louis <h078802601@gmail.com>
2021-05-22 16:58:14 +0800

    basic

---
+++ basic/sum.js
@@ -0,0 +1,12 @@
+console.log("hello");
+function sum(n){
+    let total=0
+    for(i=1;i<=n;i++){
+        total+=i
+    }
+    return total
+}
+console.log(sum(1)); // 1
+console.log(sum(2)); // 3
+console.log(sum(10)); // 55
+console.log(sum(100000)); 
