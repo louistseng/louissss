4906bbcd251b7a0f499b90e8a75a5f4e607e99f3
louis <h078802601@gmail.com>
2021-05-29 12:05:38 +0800

    update

--- app.js
+++ app.js
@@ -5,4 +5,19 @@
 // &date=20210524
 // &stockNo=2610
 
+const axios = require("axios");
+axios
+.get(
+ "https://www.twse.com.tw/exchangeReport/STOCK_DAY ?response=json &date=20210529 &stockNo=2610"
+)
+.then(function(response){
+    console.log(response);
+})
+.catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
 
