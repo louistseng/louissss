e649b9ebc5a5ee2073d205f3e231a05d4e59c721
louis <h078802601@gmail.com>
2021-06-10 16:13:32 +0800

    test promise

--- crawler/app-promise.js
+++ crawler/app-promise.js
@@ -3,40 +3,44 @@
 // &date=20210523
 // &stockNo=2610
 
+const axios = require("axios");
+const fs = require("fs");
+const moment = require("moment");
+
 // console.log(moment().format()); // 2021-05-30T13:45:06+08:00
 // console.log(moment().format("YYYYMMDD")); // 20210530
 
-const fs = require("fs");
-const axios = require("axios");
-const moment = require("moment");
-
-function filePromise() {
-  return new Promise((resolve, reject) => {
-    this.axios({
-      method:"get",
-      url:"https://www.twse.com.tw/exchangeReport/STOCK_DAY", 
-      response: "json",
-      date: moment().format("YYYYMMDD")
-    })
-    fs.readFile("stock.txt", "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(data);
+function readFilePromise () {
+    return new Promise ((resolve,reject) => {
+       fs.readFile("stock.txt","utf-8",(err,data) => {
+           if(err) {
+               reject(err);
+           }else {
+               resolve(data);
+           }
+       });
     });
-  });
 }
-filePromise()
-  .then((stock) => {
-    console.log("stockNo:", stock);
-    
-  })
-  .then(function (response) {
-    if (response.data.stat === "OK") {
-      console.log(response.data.date);
-      console.log(response.data.title);
+readFilePromise()
+.then((data) => {
+    console.log("data:",data);
+    return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: "json",
+        date: moment().format("YYYYMMDD"),
+        stockNo: data,
+      },
+    });
+})
+.then((res) => {
+    if(res.data.stat === 'OK' ){
+    console.log("資訊:" , res.data);
+      // console.log(response.data.title);
+    }else{
+        console.log("錯誤:" , err);
     }
-  })
-  .catch((err) => {
-    console.log(`錯誤:${err}`);
-  });
+})
+.catch((err) => {
+    console.error(err);
+})
+
