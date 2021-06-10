c0d5f4c3d606b962c5db1999f19f1c985ba81e78
louis <h078802601@gmail.com>
2021-06-10 23:18:13 +0800

    appMysql.js

--- crawler/appMysql.js
+++ crawler/appMysql.js
@@ -5,45 +5,110 @@ const moment = require("moment");
 const mysql = require("mysql");
 const Promise = require("bluebird");
 
-require("dotenv").config();
-
 let connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "stock",
 });
 connection = Promise.promisifyAll(connection);
 
-(async function() {
-  try{
-      await connection.connectAsync();
-  let stockCode = await fs.readFile("stock.txt", "utf-8");
-  console.log(`讀到的 stock code: ${stockCode}`);
-
-  let stock = await connection.queryAsync(`SELECT stock_id FROM stock WHERE stock_id = ${stockCode}`
-  );
-  if (stock.length <= 0) {
-      console.log("Start to query name")
-      let response = await axios.get(
-          `https://www.twse.com.tw/zh/api/codeQuery?query=${stockCode}`
-      );
-      let answer = response.data.suggestions.shift();
-      let answers = answer.split("\t");
+(async function () {
+await connection.connectAsync();
+try{
+    let stock = await fs.readFile("stock.txt", "utf8");
+    console.log("stockNo:", `${stock}`);
+    
+    let stockCode = await connection.queryAsync (`SELECT stock_id FROM stock WHERE stock_id = ${stock}`);
+    
+    if (stockCode.length <= 0){
+        let response = await axios.get(
+            `https://www.twse.com.tw/zh/api/codeQuery?query=${stock}`
+          );
+          let answer = response.data.suggestions.shift();
+        let answers = answer.split("\t");
       // console.log(stockInfos);
-      if (answers.length > 1) {
+        console.log(answers);
+        if (answers.length > 1) {
         //TODO: answers[0], answers[1]
-        connection.queryAsync(
-          `INSERT INTO stock (stock_id, stock_name) VALUES ('${answers[0]}', '${answers[1]}')` 
-        );
-      }
-     
-  }
-  } catch(err) {
-      console.error(err);
-  } finally {
-      connection.end();
-  }
-  
-  })();
+          connection.queryAsync(
+            `INSERT INTO stock (stock_id,stock_name)VALUE ('${answers[0]}','${answers[1]}')`
+          );
+        }
+    }  
+}catch(err){
+    console.error(err);
+}finally{
+    connection.end();
+}
+})();
+
+//https://www.twse.com.tw/zh/api/codeQuery?query=2610
+// fs.readFile("stock.txt", "utf8")
+//   .then((stock) => {
+//     console.log("stockNo:", stock);
+
+//     connection.query(
+//       `SELECT stock_id FROM stock WHERE stock_id = ${stock}`,
+//       function (err, result) {
+//         if (err) {
+//           throw err;
+//         } else {
+//           console.log(result.length);
+//         }
+//         if (result.length === 0) {
+//           return axios.get(
+//             `https://www.twse.com.tw/zh/api/codeQuery?query=${stock}`
+//           );
+//         }
+//       }
+//     );
+//   })
+//   .then(function (response) {
+//     let answer = response.data.suggestions.shift();
+//     let answers = answer.split("\t");
+//     console.log(answers);
+//     if (answers.length > 1) {
+//       connection.query(
+//         `INSERT INTO stock (stock_id,stock_name)VALUE ('${answers[0]}','${answers[1]}')`,
+//         function (err, result) {
+//           if (err) {
+//             throw err;
+//           } else {
+//             console.log(result);
+//           }
+//         }
+//       );
+//     } else {
+//       throw "查不到名稱";
+//     }
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   })
+//   .finally(() => {
+//     connection.end();
+//   });
+// .then((data) => {
+//     console.log("data:",data);
+//     return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+//       params: {
+//         response: "json",
+//         date: moment().format("YYYYMMDD"),
+//         stockNo: data,
+//       },
+//     });
+// })
+// .then((res) => {
+//     if(res.data.stat === 'OK' ){
+//     console.log("資訊:" , res.data);
+//       console.log(response.data.title);
+//     }else{
+//         console.log("錯誤:" , err);
+//     }
+// })
+// .catch((err) => {
+//     console.error(err);
+// })
+
+
