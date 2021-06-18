98e83c4443989d46f9dec2c96edc395baef552b1
louis <h078802601@gmail.com>
2021-06-18 22:18:09 +0800

    db.js

---
+++ crawler/db.js
@@ -0,0 +1,16 @@
+
+require("dotenv").config();
+
+//設定資料庫連線
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+//connection->Promise化
+connection = Promise.promisifyAll(connection);
+
+exports.connection = connection;
+
